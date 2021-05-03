d3bf25fe29eb0704adbd4c1e2578b85cff842433
GuillaumeDavid23 <guillaume.david744@orange.fr>
2021-05-03 10:00:29 +0200

    Exo4

---
+++ Exercice4.js
@@ -0,0 +1,9 @@
+let identity = "";
+let age = 0;
+let  city = ""
+
+indentity = prompt("Quelle est votre nom ?");
+age = prompt("Quel est votre age ?");
+city = prompt("Où habitez vous ?");
+
+alert("Bonjour je m'appelle "+indentity+" j'ai "+age+" ans"+" et j'habite "+ city);
