e02116624c0cc3bdc5e70950202ff4e093b12f26
GuillaumeDavid23 <guillaume.david744@orange.fr>
2021-05-03 09:53:28 +0200

    Exo3 corrigé

--- Exercice3.js
+++ Exercice3.js
@@ -2,4 +2,6 @@ let name = "David";
 let age = 21;
 let  city = "Paris"
 
-document.write("Bonjour je m'appelle ", name, " j'ai ", age, " et j'habite ", city);
+
+
+document.write("Bonjour je m'appelle ", name, " j'ai ", age," ans", " et j'habite ", city);12
