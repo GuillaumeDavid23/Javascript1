661a02e33f571ef8f8aaf29fefd8f1a29a685597
GuillaumeDavid23 <guillaume.david744@orange.fr>
2021-05-03 10:55:46 +0200

    Exo5

---
+++ Exercice5.js
@@ -0,0 +1,9 @@
+let numberOne = parseInt(prompt("Entrez un nombre"));
+let numberTwo = parseInt(prompt("Entrez un nombre"));
+let result = 0;
+
+
+
+result = numberOne+numberTwo;
+
+alert(numberOne + "+" + numberTwo + "=" + result);
